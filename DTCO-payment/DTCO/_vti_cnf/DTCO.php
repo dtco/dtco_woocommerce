c78040af071f1e1b7875d57e80f987837b576e9d
DTCO <developer@dtco.co>
2015-04-15 14:38:29 +0800

    v 1.0.0 by DTCO

---
+++ DTCO-payment/DTCO/_vti_cnf/DTCO.php
@@ -0,0 +1,15 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|13 Mar 2015 09:42:35 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|DTCO-ISHA-PC\\user
+vti_modifiedby:SR|DTCO-ISHA-PC\\user
+vti_timecreated:TR|12 Mar 2015 08:46:17 -0000
+vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TW|13 Mar 2015 09:42:26 -0000
+vti_cacheddtm:TX|13 Mar 2015 09:42:35 -0000
+vti_filesize:IR|493
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_charset:SR|utf-8
---
+++ DTCO-payment/_vti_cnf/DTCO.php
@@ -0,0 +1,15 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|DTCO-ISHA-PC\\user
+vti_modifiedby:SR|DTCO-ISHA-PC\\user
+vti_timelastmodified:TR|13 Mar 2015 02:55:53 -0000
+vti_timecreated:TR|13 Mar 2015 02:38:15 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TR|13 Mar 2015 02:39:45 -0000
+vti_cacheddtm:TX|13 Mar 2015 02:55:53 -0000
+vti_filesize:IR|190
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_charset:SR|utf-8
